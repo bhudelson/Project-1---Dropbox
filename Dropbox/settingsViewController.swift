52e7bdb84263a7ed462ab871064b31df11b626b3
bhudelson <bhudelso@gmail.com>
2016-02-06 20:34:03 -0800

    Project updates

---
+++ Dropbox/settingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  settingsViewController.swift
+//  Dropbox
+//
+//  Created by Blake Hudelson on 2/6/16.
+//  Copyright © 2016 Blake Hudelson. All rights reserved.
+//
+
+import UIKit
+
+class settingsViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var pictureView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       scrollView.contentSize = CGSize(width: 320, height: 800)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
